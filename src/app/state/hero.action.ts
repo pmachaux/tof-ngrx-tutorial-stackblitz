8fe921d46a049b71694dbf4feac7dfd6fbc5efd0
pmachaux <pmachaux@acquisio.com>
2020-01-21 10:40:05 -0500

    Commit begining step 2

--- src/app/state/hero.action.ts
+++ src/app/state/hero.action.ts
@@ -2,5 +2,5 @@ import {createAction, props} from "@ngrx/store";
 import {Hero} from "../hero";
 
 export const setHeroes = createAction('[HERO] set heroes', props<{heroes: Hero[]}>());
-export const addHero = createAction('[HERO] add hero', props<{hero: Hero}>());
-export const deleteHero = createAction('[HERO] delete hero', props<{hero: Hero}>());
+/*export const addHero = createAction('[HERO] add hero', props<{hero: Hero}>());
+export const deleteHero = createAction('[HERO] delete hero', props<{hero: Hero}>());*/
